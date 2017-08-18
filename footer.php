096c6c49870f1dac434c1ab66655075591ef3d11
Wilker Gaudencio <wilker@gaudit.com.br>
2017-08-18 18:37:08 -0300

    inserindo php

---
+++ footer.php
@@ -0,0 +1,47 @@
+ <!-- FOOTER -->
+<footer class="mastfoot silver-bg ">
+
+      <div class="container">
+        <div class="row">
+          <article class="col-md-1">
+            <nav class="footnav ">
+                  <ul>
+                    <li><a class="font1" href="#">Twitter</a></li>
+                    <li><a class="font1" href="#">Dribbble</a></li>
+                    <li><a class="font1" href="#">Facebook</a></li>
+                  </ul>
+            </nav>
+          </article>
+          <article class="col-md-3 text-left">
+            <h3 class="font2 black">WILKER GAUDENCIO</h3>
+            <h6 class="font1 grey">Projeto Blog Criado para auxiliar e contribuir com pessoas que querem estudar. #vemComNois.
+            </h6>
+          </article>
+          <article class="col-md-6 text-left">
+            <h3 class="font2 black">&copy;</h3>
+
+            <h6 class="font1 grey">Copyright 2017 GAUDIT.<br/><a href="http://www.gaudit.com.br/">GAUDIT</a></h6>
+          </article>
+          <article class="col-md-2 text-right">
+            <img alt="" title="" class="img-responsive foot-logo" src="images/logo.png">
+          </article>
+        </div>
+      </div>
+
+    </footer>
+    <!-- end : mastfoot -->
+
+
+    <!-- End Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <!-- JS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <script src="javascripts/libs/common.js"></script>
+    <script src="elements/js/elements.js"></script>
+    <script src="javascripts/custom/main.js"></script>
+
+</body>
+
+
+</html>
